5d01916cb71965ac960cbd3b8f543a69589637fb
Nicholas Harrison <nicholas.robin.harrison@gmail.com>
2023-01-14 14:48:58 -0500

    - add preferences & cleanup functions

--- src/bpm.tsx
+++ src/bpm.tsx
@@ -1,13 +1,20 @@
-import { Action, ActionPanel, Detail } from "@raycast/api";
+import { Action, ActionPanel, Detail, getPreferenceValues, Icon, openCommandPreferences } from "@raycast/api";
 import { useCallback, useEffect, useState } from "react";
 
+interface Preferences {
+    resetTimer: number;
+    bpmDecimalPlaces: number;
+    perBeatDecimalPlaces: number;
+}
+
 export default function Command() {
+    const preferences = getPreferenceValues<Preferences>();
     const [count, setCount] = useState(0);
     const [taps, setTaps] = useState<number>(0);
     const [timeElapsed, setTimeElapsed] = useState(0);
-    const [bpm, setBPM] = useState("0.0");
-    const [perBeat, setPerBeat] = useState("0.00");
     const [resetTimer, setResetTimer] = useState(0);
+    const [bpm, setBPM] = useState((0).toFixed(preferences.bpmDecimalPlaces));
+    const [perBeat, setPerBeat] = useState((0).toFixed(preferences.perBeatDecimalPlaces));
 
     const reset = () => {
         setTimeElapsed(0);
@@ -17,15 +24,19 @@ export default function Command() {
 
     useEffect(() => {
         if (timeElapsed) {
-            setBPM(((1000 / (timeElapsed / taps)) * 60).toFixed(1));
-            setPerBeat((timeElapsed / taps).toFixed(2));
+            setBPM(((1000 / (timeElapsed / taps)) * 60).toFixed(preferences.bpmDecimalPlaces || 1));
+            setPerBeat((timeElapsed / taps).toFixed(preferences.perBeatDecimalPlaces || 2));
         }
     }, [timeElapsed]);
 
     useEffect(() => {
         let interval: any;
+        
+        if (resetTimer >= preferences.resetTimer) {
+            reset();
+        }
 
-        if (taps && resetTimer < 2) {
+        if (taps && resetTimer < preferences.resetTimer) {
             interval = setInterval(() => {
                 setResetTimer((resetTimer) => resetTimer + 1);
             }, 1000);
@@ -34,28 +45,21 @@ export default function Command() {
         return () => clearInterval(interval);
     }, [taps, resetTimer]);
 
-    useEffect(() => {
-        if (resetTimer > 1) {
-            reset();
-            setTaps(1);
-        }
-    }, [taps]);
-
     const onTap = useCallback(() => {
         let newTaps = taps + 1;
-        setTaps(newTaps);
-        if (newTaps) {
-            const d = new Date();
-            const increment = timeElapsed + (d.valueOf() - count);
-            if (newTaps === 2) {
-                // doubles the first "timeElapsed" value
-                // to simulate the missing interval from 0-1
-                setTimeElapsed(increment * 2);
-            } else {
-                count && setTimeElapsed(increment);
-            }
-            setCount(d.valueOf());
+        const d = new Date();
+        const increment = timeElapsed + (d.valueOf() - count);
+
+        if (newTaps === 2) {
+            // doubles the first "timeElapsed" value
+            // to simulate the missing interval from 0-1
+            setTimeElapsed(increment * 2);
+        } else {
+            count && setTimeElapsed(increment);
         }
+
+        setTaps(newTaps);
+        setCount(d.valueOf());
         setResetTimer(0);
     }, [taps]);
 
@@ -64,10 +68,21 @@ export default function Command() {
             markdown={`# ${bpm}`}
             metadata={
                 <Detail.Metadata>
-                    <Detail.Metadata.Label title="Beats" text={taps.toString()} />
-                    <Detail.Metadata.Label title="MS per Beat" text={perBeat} />
-                    <Detail.Metadata.Label title="BPM reset: 2 seconds" text="(or hit shift + 'r')" />
-                    <Detail.Metadata.Label title={resetTimer > 1 ? "BPM will reset next tap" : ""} />
+                    <Detail.Metadata.Label 
+                        title="Beats"
+                        text={taps.toString()}
+                    />
+                    <Detail.Metadata.Label
+                        title="MS per Beat"
+                        text={perBeat}
+                    />
+                    <Detail.Metadata.Label
+                        title={`BPM reset: ${preferences.resetTimer} seconds`} 
+                        text="(or hit shift + 'r')"
+                    />
+                    <Detail.Metadata.Label
+                        title={resetTimer >= preferences.resetTimer ? "BPM will reset on next tap" : ""}
+                    />
                 </Detail.Metadata>
             }
             actions={
@@ -75,8 +90,20 @@ export default function Command() {
                     <Action
                         title="Tap"
                         onAction={onTap}
+                        icon={Icon.Heartbeat}
                     />
-                    <Action title="Reset" shortcut={{ modifiers: ["shift"], key: "r" }} onAction={reset} />
+                    <Action 
+                        title="Reset"
+                        shortcut={{ modifiers: ["shift"], key: "r" }}
+                        onAction={reset}
+                        icon={Icon.ArrowCounterClockwise}
+                    />
+                    <Action 
+                        title="Preferences"
+                        shortcut={{ modifiers: ["shift"], key: '/' }}
+                        onAction={openCommandPreferences}
+                        icon={Icon.Gear}
+                    />
                 </ActionPanel>
             }
         />
